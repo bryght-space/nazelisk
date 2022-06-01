5a62cdd5b9a4eda95c67e83dc60aef4804175e5d
Marc Esquerra <esquerra@gmail.com>
2022-06-01 15:48:53 +0100

    add bazel5

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@ with (import ./helpers.nix);
 let
   getByRevision = revision: name: builtins.getAttr name (channelFromRevision revision);
   revisions = {
+    "5.1.1" = ["af55be9eace60ddc1346e39a3d98cfae8ec506eb" "bazel_5" "jdk11" ];
     "4.0.0" = ["d4590d21006387dcb190c516724cb1e41c0f8fdf" "bazel_4" "jdk11" ];
     "3.7.2" = ["0b9f90ef5f2c5dabafa12e8d8a5e549ad901149e" "bazel_3" "jdk8"  ];
     "3.7.1" = ["71cda4f1118d41db29e50077fb8611f9ac472b8e" "bazel_3" "jdk8"  ];
